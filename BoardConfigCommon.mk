8706308024718be3243b344a93ede943fce85c61
Rian Souza Santos <rssxda.dev@icloud.com>
2023-12-27 22:29:36 -0300

    sm6225-common: revert: use kernel-headers, qcom hardware
    This reverts commit ff48b110705ce591ed29506bddbf2a6d19139aff.
    
    Signed-off-by: Rian Souza Santos <rssxda.dev@icloud.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,14 +91,50 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=2048
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_CONFIG := vendor/bengal-perf_defconfig vendor/debugfs.config vendor/ext_config/moto-bengal.config
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6225
 
-# QCOM
-ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
-  include $(COMMON_PATH)/hardware/qcom/board.mk
-endif
-TARGET_USES_HARDWARE_QCOM_GPS := false
+# Kernel Modules - Audio
+TARGET_MODULE_ALIASES += \
+    adsp_loader_dlkm.ko:audio_adsp_loader.ko \
+    apr_dlkm.ko:audio_apr.ko \
+    bolero_cdc_dlkm.ko:audio_bolero_cdc.ko \
+    machine_dlkm.ko:audio_machine_bengal.ko \
+    mbhc_dlkm.ko:audio_mbhc.ko \
+    native_dlkm.ko:audio_native.ko \
+    pinctrl_lpi_dlkm.ko:audio_pinctrl_lpi.ko \
+    platform_dlkm.ko:audio_platform.ko \
+    pm2250_spmi_dlkm.ko:audio_pm2250_spmi.ko \
+    q6_dlkm.ko:audio_q6.ko \
+    q6_notifier_dlkm.ko:audio_q6_notifier.ko \
+    q6_pdr_dlkm.ko:audio_q6_pdr.ko \
+    rouleur_dlkm.ko:audio_rouleur.ko \
+    rouleur_slave_dlkm.ko:audio_rouleur_slave.ko \
+    rx_macro_dlkm.ko:audio_rx_macro.ko \
+    snd_event_dlkm.ko:audio_snd_event.ko \
+    aw882xx_dlkm.ko:audio_snd_soc_aw882xx.ko \
+    stub_dlkm.ko:audio_stub.ko \
+    swr_dlkm.ko:audio_swr.ko \
+    swr_ctrl_dlkm.ko:audio_swr_ctrl.ko \
+    tx_macro_dlkm.ko:audio_tx_macro.ko \
+    usf_dlkm.ko:audio_usf.ko \
+    va_macro_dlkm.ko:audio_va_macro.ko \
+    wcd937x_dlkm.ko:audio_wcd937x.ko \
+    wcd937x_slave_dlkm.ko:audio_wcd937x_slave.ko \
+    wcd9xxx_dlkm.ko:audio_wcd9xxx.ko \
+    wcd_core_dlkm.ko:audio_wcd_core.ko \
+    wsa881x_analog_dlkm.ko:audio_wsa881x_analog.ko
+
+# Kernel Modules - WLAN
+TARGET_MODULE_ALIASES += \
+    wlan.ko:qca_cld3_wlan.ko
+
+# Partitions
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_USES_METADATA_PARTITION := true
@@ -127,6 +163,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
 TARGET_BOARD_PLATFORM := bengal
+BOARD_USES_QCOM_HARDWARE := true
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/fsg:/fsg
